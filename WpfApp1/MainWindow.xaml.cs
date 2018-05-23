41a12db40cf88e431edbc962a15c4b4ea7c0b047
Yuriy Ivaskevych <ivaskevychuriy@gmail.com>
2018-05-24 02:15:10 +0300

    Add physical coefs; Add DFIABG

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using HelixToolkit.Wpf;
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
@@ -17,7 +18,10 @@ namespace WpfApp1
         private readonly Brush LineFill = Brushes.LightGray;
         private const double VertRadius = 0.1;
 
-        private const int FEPointsCount = 20;
+        private const double E = 1.0;
+        private const double v = 1.0;
+        private const double l = E / ((1 + v) * (1 - 2 * v));
+        private const double m = E / (2 * (1 + v));
 
         public MainWindow()
         {
@@ -28,7 +32,7 @@ namespace WpfApp1
             const int nz = 2;
 
             // initial points array
-            var AKT = CalculateAKT(nx, ny, nz);
+            var AKT = GenerateAKT(nx, ny, nz);
             Render(AKT);
 
             // TODO: calculate NT
@@ -37,15 +41,17 @@ namespace WpfApp1
             // fixed points global coords (ZU) and force info (ZP)
             var ZU = CalculateZU(nx, ny, nz);
             var ZP = CalculateZP(nx, ny, nz);
+
+            var DFIABG = GenerateDFIABG();
         }
-        
+
         private void Render(Point3D[] points)
         {
             // TODO: add edges
             GenerateAndAddShapeToCollection(points, new int[] { }, Brushes.Blue, MainViewPort.Children);
         }
 
-        private Point3D[] CalculateAKT(int nx, int ny, int nz)
+        private Point3D[] GenerateAKT(int nx, int ny, int nz)
         {
             int cx = nx + 1;
             int cy = ny + 1;
@@ -120,6 +126,101 @@ namespace WpfApp1
 #endif
 
             return ZP;
+        }
+
+        private double[,,] GenerateDFIABG()
+        {
+            // generate 27 points in -1 to 1 standard cube 
+            var gaussPoints = new Point3D[27];
+            int c = 0;
+            for (int cx = 0; cx < 3; ++cx)
+            {
+                for (int cy = 0; cy < 3; ++cy)
+                {
+                    for (int cz = 0; cz < 3; ++cz)
+                    {
+                        gaussPoints[c++] = new Point3D(-1.0 + cx * 0.5, -1.0 + cy * 0.5, -1.0 + cz * 0.5);
+                    }
+                }
+            }
+
+            // 20 points for alpha_i, beta_i and gamma_i
+            var points = new Point3D[20]
+            {
+                // first 8
+                new Point3D(-1, -1, -1),
+                new Point3D(1, -1, -1),
+                new Point3D(1, 1, -1),
+                new Point3D(-1, 1, -1),
+                new Point3D(-1, -1, 1),
+                new Point3D(1, -1, 1),
+                new Point3D(1, 1, 1),
+                new Point3D(-1, 1, 1),
+
+                // rest 12
+                new Point3D(0, -1, -1),
+                new Point3D(1, 0, -1),
+                new Point3D(0, 1, -1),
+                new Point3D(-1, 0, -1),
+                new Point3D(-1, -1, 0),
+                new Point3D(1, -1, 0),
+                new Point3D(1, 1, 0),
+                new Point3D(-1, 1, 0),
+                new Point3D(0, -1, 1),
+                new Point3D(1, 0, 1),
+                new Point3D(0, 1, 1),
+                new Point3D(-1, 0, 1)
+            };
+
+            // functions for dimensions for i < 8
+            var dphis1 = new Func<Point3D, Point3D, double>[3]
+            {
+                (Point3D p, Point3D pi) => pi.X * (1 + p.Y * pi.Y) * (1 + p.Z * pi.Z) * (2 * p.X * pi.X + p.Y * pi.Y + p.Z * pi.Z - 1) / 8.0,
+                (Point3D p, Point3D pi) => pi.Y * (1 + p.X * pi.X) * (1 + p.Z * pi.Z) * (2 * p.Y * pi.Y + p.X * pi.X + p.Z * pi.Z - 1) / 8.0,
+                (Point3D p, Point3D pi) => pi.Z * (1 + p.X * pi.X) * (1 + p.Y * pi.Y) * (2 * p.Z * pi.Z + p.X * pi.X + p.Y * pi.Y - 1) / 8.0
+            };
+
+            // functions for dimensions for i >= 8
+            var dphis2 = new Func<Point3D, Point3D, double>[3]
+            {
+                (Point3D p, Point3D pi) => (1 + p.Y * pi.Y) * (1 + p.Z * pi.Z) * (pi.Y * pi.Z * pi.Z + pi.X * pi.X * (pi.Y * pi.Y * p.Z + p.Y * pi.Z * pi.Z) + pi.X * (2 * pi.Y * p.X * pi.Z * pi.Z - 1)) / -4.0,
+                (Point3D p, Point3D pi) => (1 + p.X * pi.X) * (1 + p.Z * pi.Z) * (pi.X * pi.Y * pi.Y * pi.Y * p.Z + pi.X * pi.Z * pi.Z + p.X * pi.Y * pi.Y * pi.Z * pi.Z + pi.Y * (2 * pi.X * p.Y * pi.Z * pi.Z - 1)) / -4.0,
+                (Point3D p, Point3D pi) => (1 + p.X * pi.X) * (1 + p.Y * pi.Y) * (pi.X * p.Y * pi.Z * pi.Z * pi.Z + pi.X * pi.Y * pi.Y * (1 + 2 * p.Z * pi.Z) + pi.Z * (p.X * pi.Y * pi.Z * pi.Z - 1)) / -4.0
+            };
+
+            // calculate DFIABG itself
+            var result = new double[27,3,20];
+            for (int cg = 0; cg < 27; ++cg)             // gauss points
+            {
+                var p = gaussPoints[cg];
+                for (int d = 0; d < 3; ++d)             // dimension
+                {
+                    for (int i = 0; i < 8; ++i)        // functions < 8
+                    {
+                        result[cg, d, i] = dphis1[d](p, points[i]);
+                    }
+                    for (int i = 8; i < 20; ++i)        // functions >= 8
+                    {
+                        result[cg, d, i] = dphis2[d](p, points[i]);
+                    }
+                }
+            }
+
+#if DEBUG
+            Debug.WriteLine("DFIABG: ");
+            for (int cg = 0; cg < 27; ++cg)
+            {
+                for (int d = 0; d < 3; ++d)
+                {
+                    for (int i = 0; i < 20; ++i)
+                    {
+                        Debug.WriteLine(result[cg, d, i]);
+                    }
+                }
+            }
+#endif
+
+            return result;
         }
 
         private void GenerateAndAddShapeToCollection(Point3D[] points, int[] pointIndexes, Brush vertColor, Visual3DCollection collection)
