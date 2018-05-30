2ad2e3009b598e287ff961682a7af6f1da9953ae
Maksym Lema <maksym.lema@skaveetech.com>
2018-05-31 01:30:39 +0300

    Gausus method po bratski ne obezsut

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -51,6 +51,8 @@ namespace WpfApp1
             var DPSITE = GenerateDPSITE.Generate();
 
             var (MG, F) = ProcessElements(nx, ny, nz, AKT, NT, DFIABG, DPSITE);
+
+            var somehell = GaussianElimination(MG, F);
         }
 
         private void Render(Point3D[] points)
@@ -479,6 +481,62 @@ namespace WpfApp1
             //}
         }
 
+        private double[,] GaussianElimination(double[,] A, double[] x)
+        {
+            int n = x.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                // Search for maximum in this column
+                double maxEl = Math.Abs(A[i, i]);
+                int maxRow = i;
+                for (int k = i + 1; k < n; k++)
+                {
+                    if (Math.Abs(A[k, i]) > maxEl)
+                    {
+                        maxEl = Math.Abs(A[k, i]);
+                        maxRow = k;
+                    }
+                }
+
+                // Swap maximum row with current row (column by column)
+                for (int k = i; k < n + 1; k++)
+                {
+                    double tmp = A[maxRow, k];
+                    A[maxRow, k] = A[i, k];
+                    A[i, k] = tmp;
+                }
+
+                // Make all rows below this one 0 in current column
+                for (int k = i + 1; k < n; k++)
+                {
+                    double c = -A[k, i] / A[i, i];
+                    for (int j = i; j < n + 1; j++)
+                    {
+                        if (i == j)
+                        {
+                            A[k, j] = 0;
+                        }
+                        else
+                        {
+                            A[k, j] += c * A[i, j];
+                        }
+                    }
+                }
+            }
+
+            // Solve equation Ax=b for an upper triangular matrix A
+            for (int i = n - 1; i >= 0; i--)
+            {
+                x[i] = A[i, n] / A[i, i];
+                for (int k = i - 1; k >= 0; k--)
+                {
+                    A[k, n] -= A[k, i] * x[i];
+                }
+            }
+
+            return A;
+        }
         private PipeVisual3D CreateLine(Point3D start, Point3D end)
         {
             var line = new PipeVisual3D();
