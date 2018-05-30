a626ea2a94655717584ef71df9cfb93adaf0f731
Yuriy Ivaskevych <ivaskevychuriy@gmail.com>
2018-05-31 01:46:15 +0300

    Add FixMG

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace WpfApp1
         private const double l = E / ((1 + v) * (1 - 2 * v));
         private const double m = E / (2 * (1 + v));
 
+        private readonly double LargeCoefficient = Math.Pow(10, 50);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -51,6 +53,7 @@ namespace WpfApp1
             var DPSITE = GenerateDPSITE.Generate();
 
             var (MG, F) = ProcessElements(nx, ny, nz, AKT, NT, DFIABG, DPSITE);
+            FixMG(MG, ZU);
         }
 
         private void Render(Point3D[] points)
@@ -459,6 +462,18 @@ namespace WpfApp1
 #endif
 
             return result;
+        }
+        
+        private void FixMG(double[,] MG, int[] ZU)
+        {
+            for (int d = 0; d < 3; ++d)
+            {
+                foreach (var i in ZU)
+                {
+                    int index = 3 * i + d;
+                    MG[index, index] = LargeCoefficient;
+                }
+            }
         }
 
         private void GenerateAndAddShapeToCollection(Point3D[] points, int[] pointIndexes, Brush vertColor, Visual3DCollection collection)
