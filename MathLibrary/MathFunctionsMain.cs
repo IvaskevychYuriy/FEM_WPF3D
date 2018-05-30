86fc06299a382cbaed499fc1b8cd6bcbce1e05e5
Andriy Bilovus <andrewbilovus@gmail.com>
2018-05-31 00:18:45 +0300

    added dpsite

---
+++ MathLibrary/MathFunctionsMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathLibrary
+{
+    public class MathFunctionsMain
+    {
+        //public static double[,] GenerateNTMatrix(double ax, double ay, double az, int nx, int ny, int nz)
+        //{
+        //    int cubesNumber = nx * ny * nz;
+        //    var matrix = new double[20, cubesNumber];
+        //    for (int i = 0; i < cubesNumber; i++)
+        //    {
+
+        //    }
+        //}
+
+        //public static Dictionary<int, Point> GenerateSitka(double ax, double ay, double az, int nx, int ny, int nz)
+        //{
+        //    int cubesNumber = nx * ny * nz;
+        //    var sitka = new Dictionary<int, Point>();
+
+        //    double deltaX = ax / nx;
+
+        //    for (int ix = 1; ix <= nx * 2 + 1; ix++)
+        //    {
+        //        for (int iy = 1; iy <= ny * 2 + 1; iy++)
+        //        {
+        //            for (int iz = 1; iz <= nz * 2 + 1; iz++)
+        //            {
+        //                if (iz % 2 == 1)
+        //                {
+        //                    sitka.Add(ix * 2 - 1, new Point(deltaX * ix - deltaX / 2), )
+        //                    sitka.Add(ix * 2, new Point(deltaX * ix))
+        //                }
+        //                else
+        //                {
+
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
